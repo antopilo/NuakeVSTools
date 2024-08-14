2fed02ee1257225e6b1099f78fea83aa4ced1f8a
Antoine Pilote <antoinep@momentfactory.com>
2024-08-14 13:08:19 -0400

    Added basic entity script item template

---
+++ NuakeCSharpTemplate/Class.cs
@@ -0,0 +1,38 @@
+﻿using Nuake;
+using System;
+
+namespace $rootnamespace$
+{
+	public class $safeitemrootname$ : Entity
+	{
+
+		/// <summary>
+		/// OnInit is called once when the entity is instantiated
+		/// </summary>
+		public override void OnInit()
+		{
+		}
+
+		/// <summary>
+		/// OnUpdate is called every frame
+		/// </summary>
+		public override void OnUpdate(float dt)
+		{
+		}
+
+		/// <summary>
+		/// OnFixed update is called every physics frame
+		/// </summary>
+		/// <param name="dt"></param>
+		public override void OnFixedUpdate(float dt)
+		{
+		}
+
+		/// <summary>
+		/// OnDestroy is called when the entity is removed from the scene
+		/// </summary>
+		public override void OnDestroy()
+		{
+		}
+	}
+}
